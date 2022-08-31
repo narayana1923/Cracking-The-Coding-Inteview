73237a32739ec2813d3234a828d138fad3c9f0b5
Lakshmi Narayana Velayudam <lcchinnu@gmail.com>
2022-08-31 16:13:49 +0530

    added string rotation

--- Arrays-string/string-rotation.cpp
+++ Arrays-string/string-rotation.cpp
@@ -3,7 +3,23 @@
 using namespace std;
 
 bool is_substring(string& s1,string& s2){
-    
+    if(s1.size() != s2.size()) return false;
+    string temp = s1+s1;
+    int m = temp.size();
+    int n = s2.size();
+    for(int i=0;i<m;i++){
+        if(temp[i]==s2[0]){
+            bool flag = true;
+            for(int j=0;j<n;j++){
+                if(temp[i+j]!=s2[j]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) return true;
+        }
+    }
+    return false;
 }
 
 int main() {
